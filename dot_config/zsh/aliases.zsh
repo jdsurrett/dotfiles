0c75cdbdef906aae38d9885f10573f0f2bdce811
generic-handle4242 <jdsurrett@gmail.com>
2022-05-13 09:43:05 -0700

    Update dot_config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -5,16 +5,7 @@ pamac () {
     /usr/bin/pamac $@ | lolcat -i -p 9.0
 }
 
-tree () {
-    tput clear
-    /usr/bin/lsd --tree \
-        --date="+ ║ %D ┊ %I:%M %p ║" \
-        --group-dirs=last \
-        --blocks=date \
-        --blocks=name \
-        --blocks=size $@
-    }
-
+alias tree="tput clear && lsd --tree --date='+ ║ %D ┊ %I:%M %p ║' --group-dirs=last --blocks=date --blocks=name --blocks=size"
 alias cargo="tput clear && cargo"
 alias vim="lvim"
 alias vi="lvim"
