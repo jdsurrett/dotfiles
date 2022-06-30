2f971d1d7ecd0de13b6ade667e816a195b97e5f7
generic-handle4242 <jdsurrett@gmail.com>
2022-06-30 10:42:04 -0700

    Stuff

--- home/.chezmoiscripts/run_once_install-packages.zsh
+++ home/.chezmoiscripts/run_once_install-packages.zsh
@@ -19,8 +19,10 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $CARGO_HOME/env
 rm -f $HOME/.profile $ZDOTDIR/.zshenv
 
+cargo install fd-find taplo-cli
 cargo install taplo-cli
 cargo install --locked bat
+cargo install --locked code-minimap
 
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 rm -rf $XDG_CONFIG_HOME/lvim/*
