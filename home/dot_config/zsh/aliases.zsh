52a41f723c880f4eab58b0c6be16eff324185f39
generic-handle4242 <jdsurrett@gmail.com>
2022-07-11 08:33:32 -0700

    Update home/dot_config/zsh/aliases.zsh

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -4,7 +4,7 @@ pamac () {
     tput clear
     /usr/bin/pamac $@ | lolcat -i -p 9.0
 }
-
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
 alias tree="lsd --tree --date='+ ║ %D ┊ %I:%M %p ║' --group-dirs=last --blocks=date --blocks=name --blocks=size"
 alias cargo="cargo"
 alias vim="lvim"
